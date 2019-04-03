f8abd2ef3c0e457ca67a31141fd33d991dfb37ee
lee534666Lj <534666058@qq.com>
2019-04-03 18:20:15 +0800

    第一次提交

---
+++ src/main/java/com/neu/dyd/DydApplication.java
@@ -0,0 +1,21 @@
+package com.neu.dyd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @DO:
+ * @Program:pspenv
+ * @Author 李君（2765395275）
+ * @Create: 2019/4/3 18:12
+ *--学海无涯苦作舟--
+ */
+@SpringBootApplication
+@EnableAutoConfiguration
+public class DydApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DydApplication.class, args);
+    }
+}
---
+++ src/test/java/com/neu/dyd/DydApplicationTest.java
@@ -0,0 +1,6 @@
+package com.neu.dyd;
+
+
+public class DydApplicationTest {
+
+}
